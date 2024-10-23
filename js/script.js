49eff781af03036868948e18782dff9ffe7c6456
Alexandra Kiseleva <sahsakis2512@gmail.com>
2024-10-23 20:42:44 +0400

    Connected Swiper

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
-new Swiper('.swiper', {
-
+var swiper = new Swiper(".swiper", {
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
     clickable: true,
   },
 
   mousewhell: true,
   keyboard: true,
-});
+})
