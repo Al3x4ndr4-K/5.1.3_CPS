6be95f8f0fcaae0c963d35b780f51a88eba926c5
Alexandra Kiseleva <sahsakis2512@gmail.com>
2024-10-23 23:15:40 +0400

    Before Expand-button experiment

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 if (window.innerWidth <= 767) {
   var swiper = new Swiper(".swiper", {
-    slidesPerView: 'auto',
+    slidesPerView: "auto",
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
@@ -9,4 +9,25 @@ if (window.innerWidth <= 767) {
     mousewhell: true,
     keyboard: true,
   })
-};
+}
+
+const descriptionContinueButton = document.querySelector('.description-continue-button');
+const slides = document.querySelectorAll('.brands-services__slide');
+
+descriptionContinueButton.addEventListener('click', () => {
+  if (descriptionContinueButton.classList.contains('show-less')) {
+    slides.forEach((slide, index) => {
+      if (index >= 6) {
+        slide.style.display = 'none';
+      }
+    });
+    descriptionContinueButton.classList.remove('show-less');
+  } else {
+    slides.forEach((slide, index) => {
+      if (index >= 6) {
+        slide.style.display = 'block';
+      }
+    });
+    descriptionContinueButton.classList.add('show-less');
+  }
+});
